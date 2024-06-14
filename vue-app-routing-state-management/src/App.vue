71b72809a9bd422dba0700291a7f26f976c8a84a
HarshitJumbaya <hb@jumbaya.com>
2024-06-14 13:02:04 +0530

    Pushing the vue.js app with routing and state management by Zustand

---
+++ vue-app-routing-state-management/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+
+    <RouterView />
+  </div>
+</template>
+
+
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+
+export default {
+  name: 'App'
+}
+</script>
+
+
+<style scoped>
+</style>
+
+
+<!-- <template>
+  <div id="app">
+    
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script  lang="ts">
+import {RouterLink, RouterView} from 'vue-router';
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  gap: 10px;
+}
+</style> -->
---
+++ vue-app-routing-state-management/src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <h1>Second page</h1>
+    <h3>Single: {{ count }}</h3>
+    <h3>Double: {{ doubleCount }}</h3>
+    <button @click="increaseSingleCount">Single</button>
+    <button @click="increaseDoubleCount">Double</button>
+
+  </template>
+  
+  <script setup lang="ts">
+
+  import { ref } from 'vue';
+  import { getState, setState, subscribe } from '../store';
+  
+  const count = ref(getState().count);
+  const doubleCount = ref(getState().doubleCount);
+  
+  function increaseSingleCount() {
+    setState({ count: getState().count + 1 });
+  }
+  
+  function increaseDoubleCount() {
+    setState({ doubleCount: getState().doubleCount + 2 });
+  }
+  
+  subscribe(() => {
+    count.value = getState().count;
+    doubleCount.value = getState().doubleCount;
+  });
+  
+  </script>
